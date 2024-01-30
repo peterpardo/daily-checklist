9d1bd95c1845db9032bd6d472ac319ff33b86d34
Peter Pardo <peterpardo123@gmail.com>
2024-01-30 23:15:07 +0800

    feat: added clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 
@@ -21,22 +22,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable,
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            'min-h-screen bg-background font-sans antialiased',
+            fontSans.variable,
+          )}
         >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,41 @@
 import { ThemeToggle } from '@/components/ThemeToggle';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import React from 'react';
+import { buttonVariants } from '@/components/ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
-export default function Navbar() {
+import { currentUser } from '@clerk/nextjs';
+
+import Link from 'next/link';
+
+export default async function Navbar() {
+  const user = await currentUser();
+  let avatarFallbackText = 'U';
+  if (user && user?.firstName && user?.lastName) {
+    avatarFallbackText = (
+      user?.firstName.charAt(0) + user.lastName.charAt(0)
+    ).toUpperCase();
+  }
+
   return (
     <nav className="w-full border-b px-2 py-4">
       <div className="container flex items-center justify-between">
         <Link href="/" className="font-bold text-xl">
           DailyChecklist
         </Link>
-        <div className="flex items-center gap-x-2">
+        <div className="flex items-center gap-x-5">
           <ThemeToggle />
-          <Button>Sign in</Button>
+          {user ? (
+            <Avatar>
+              <AvatarImage src={user.hasImage ? user.imageUrl : ''} />
+              <AvatarFallback>{avatarFallbackText}</AvatarFallback>
+            </Avatar>
+          ) : (
+            <Link
+              href="/sign-in"
+              className={buttonVariants({ variant: 'default' })}
+            >
+              Sign in
+            </Link>
+          )}
         </div>
       </div>
     </nav>
