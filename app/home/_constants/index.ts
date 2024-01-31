c92c38996bf0a492f2a57e8df21cc9e8e5186544
Peter Pardo <peterpardo123@gmail.com>
2024-01-31 16:25:13 +0800

    feat: added home side navbar

---
+++ app/home/_constants/index.ts
@@ -0,0 +1,19 @@
+import { CreditCard, Home, Settings } from 'lucide-react';
+
+export const navItems = [
+  {
+    name: 'Home',
+    link: '/home',
+    icon: Home,
+  },
+  {
+    name: 'Settings',
+    link: '/settings',
+    icon: Settings,
+  },
+  {
+    name: 'Billing',
+    link: '/billing',
+    icon: CreditCard,
+  },
+];
