d14c6bee9caa2d30862e6907d7991fb65ec3b46a
Peter Pardo <peterpardo123@gmail.com>
2024-01-31 17:28:24 +0800

    feat: added items in navbar menu

--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -1,13 +1,18 @@
 'use client';
 
+import { navItems } from '@/app/home/_constants';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { useClerk, useUser } from '@clerk/nextjs';
+import { LogOut } from 'lucide-react';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 export default function NavbarMenu() {
@@ -26,14 +31,29 @@ export default function NavbarMenu() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Avatar>
+        <Avatar className="cursor-pointer">
           <AvatarImage src={user?.hasImage ? user.imageUrl : ''} />
           <AvatarFallback>{avatarFallbackText}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem onClick={() => signOut(() => router.push('/'))}>
-          Logout
+      <DropdownMenuContent className="w-40">
+        <DropdownMenuGroup>
+          {navItems.map((item, i) => (
+            <DropdownMenuItem asChild key={i}>
+              <Link href={item.link} className="cursor-pointer">
+                <item.icon className="mr-2 h-4 w-4" />
+                <span>{item.name}</span>
+              </Link>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => signOut(() => router.push('/'))}
+          className="cursor-pointer"
+        >
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Logout</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
