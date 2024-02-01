e69f6fdd8ba7e50f2e3ae09cf8a43052e46c5934
Peter Pardo <peterpardo123@gmail.com>
2024-02-02 00:59:49 +0800

    feat: added add task dialog

--- app/home/[id]/page.tsx
+++ app/home/[id]/page.tsx
@@ -1,9 +1,7 @@
 import { Separator } from '@/components/ui/separator';
 import TaskCard from '@/components/TaskCard';
 import taskData from '@/tasksData.json';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import { Plus } from 'lucide-react';
+import AddTaskDialog from '@/components/AddTaskDialog';
 
 export default function CollectionPage({ params }: { params: { id: string } }) {
   const completedTasks = taskData.filter((task) => task.completed);
@@ -13,12 +11,7 @@ export default function CollectionPage({ params }: { params: { id: string } }) {
     <div className="space-y-5">
       <div className="flex gap-y-3 items-center justify-between">
         <h1 className="font-bold text-xl md:text-4xl">Monday Tasks</h1>
-        <Button variant="secondary" asChild>
-          <Link href={`/home/${params.id}/create`}>
-            <Plus size={20} className="mr-2" />
-            <span className="text-xs md:text-base">Add Task</span>
-          </Link>
-        </Button>
+        <AddTaskDialog />
       </div>
 
       <div className="max-w-[40rem] space-y-5">
