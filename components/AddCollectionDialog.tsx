97a6559f5a7877a549c1ceb5345e2c0cdc377f7f
Peter Pardo <peterpardo123@gmail.com>
2024-02-04 23:43:01 +0800

    feat: add toast on adding collection

--- components/AddCollectionDialog.tsx
+++ components/AddCollectionDialog.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
@@ -9,12 +11,62 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { FolderPlus } from 'lucide-react';
+import { useToast } from '@/components/ui/use-toast';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FolderPlus, Loader2 } from 'lucide-react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+const formSchema = z.object({
+  name: z.string().min(2).max(20),
+});
 
 export default function AddCollectionDialog() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const { toast } = useToast();
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
+
+    await (async () => {
+      return new Promise((resolve, reject) =>
+        setTimeout(() => resolve('resolved'), 1000),
+      );
+    })();
+
+    console.log(values);
+
+    toast({
+      title: 'Collection successfully created.',
+    });
+    setIsOpen(false);
+    setIsLoading(false);
+  }
+
   return (
-    <Dialog>
+    <Dialog
+      onOpenChange={(value) => {
+        form.reset();
+        setIsOpen(value);
+      }}
+      open={isOpen}
+    >
       <DialogTrigger asChild>
         <Button variant="secondary">
           <FolderPlus size={20} className="mr-2" />
@@ -29,17 +81,36 @@ export default function AddCollectionDialog() {
             kickstart your focused productivity journey!
           </DialogDescription>
         </DialogHeader>
-        <div className="flex items-center space-x-2">
-          <div className="grid flex-1 gap-2">
-            <Label htmlFor="name" className="sr-only">
-              Name
-            </Label>
-            <Input id="name" placeholder="Your collection name here..." />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Create</Button>
-        </DialogFooter>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-y-2"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="sr-only">Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Your collection name here..."
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit" disabled={isLoading}>
+                {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                {isLoading ? 'Creating..' : 'Create'}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
