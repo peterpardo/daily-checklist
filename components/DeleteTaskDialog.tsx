1cf4dc0cec04ebcdad94ed3f03f8b21e3998744a
Peter Pardo <peterpardo123@gmail.com>
2024-02-05 13:00:28 +0800

    feat: added loading state when deleting tasks

--- components/DeleteTaskDialog.tsx
+++ components/DeleteTaskDialog.tsx
@@ -10,11 +10,36 @@ import {
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
-import { Trash2 } from 'lucide-react';
+import { useToast } from '@/components/ui/use-toast';
+import { Loader2, Trash2 } from 'lucide-react';
+import { useState } from 'react';
 
 export function DeleteTaskDialog() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const { toast } = useToast();
+
+  async function handleSubmit(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    await (async () => {
+      return new Promise((resolve, reject) =>
+        setTimeout(() => resolve('resolved'), 1000),
+      );
+    })();
+
+    toast({
+      title: 'Task successfully deleted.',
+    });
+    setIsOpen(false);
+    setIsLoading(false);
+  }
+
   return (
-    <AlertDialog>
+    <AlertDialog onOpenChange={(open) => setIsOpen(open)} open={isOpen}>
       <AlertDialogTrigger asChild>
         <Button variant="ghost" size="icon">
           <Trash2 size={16} />
@@ -30,7 +55,12 @@ export function DeleteTaskDialog() {
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction asChild>
+            <Button onClick={handleSubmit} disabled={isLoading}>
+              {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              {isLoading ? 'Deleting...' : 'Continue'}
+            </Button>
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
