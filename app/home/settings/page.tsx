95244f0dbe12f78a46c2b9fadbb68455aae38ab8
Peter Pardo <peterpardo123@gmail.com>
2024-02-05 19:45:25 +0800

    feat: added settings page

--- app/home/settings/page.tsx
+++ app/home/settings/page.tsx
@@ -1,7 +1,119 @@
-import React from 'react'
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useUser } from '@clerk/nextjs';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import React from 'react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useToast } from '@/components/ui/use-toast';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  theme: z.string({
+    required_error: 'Please select a theme to apply.',
+  }),
+});
 
 export default function SettingsPage() {
+  const { user } = useUser();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+  const { toast } = useToast();
+  const emailData = user?.emailAddresses.find(
+    (email) => email.id === user.primaryEmailAddressId,
+  );
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
   return (
-    <div>Settings</div>
-  )
+    <div className="space-y-5">
+      <div className="flex gap-y-3 items-center justify-between">
+        <h1 className="font-bold text-2xl md:text-4xl">Settings</h1>
+      </div>
+
+      <p className="text-xs max-w-[50rem] md:text-sm">
+        Manage your account details here. You can also change the theme of your
+        account.
+      </p>
+
+      <div className="max-w-[40rem] space-y-5">
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            placeholder="Email"
+            disabled
+            value={emailData?.emailAddress ?? ''}
+            className="mt-1.5"
+          />
+        </div>
+
+        <div className="grid w-full items-center gap-1.5">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="theme"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Theme</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a theme" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="apple">Apple</SelectItem>
+                        <SelectItem value="banana">Banana</SelectItem>
+                        <SelectItem value="blueberry">Blueberry</SelectItem>
+                        <SelectItem value="grapes">Grapes</SelectItem>
+                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                      </SelectContent>
+                    </Select>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="mt-5 flex justify-end">
+                <Button type="submit">Submit</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 }
