c946aa4061c4c97a3c518adb77832d4990efc2d4
Peter Pardo <peterpardo123@gmail.com>
2024-02-05 12:58:20 +0800

    feat: added loading state when deleting collections

--- components/DeleteCollectionDialog.tsx
+++ components/DeleteCollectionDialog.tsx
@@ -12,16 +12,43 @@ import {
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/use-toast';
 import { DropdownMenuItem } from '@radix-ui/react-dropdown-menu';
-import { Trash } from 'lucide-react';
-import React, { Ref } from 'react';
+import { Loader2, Trash } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React, { Ref, useState } from 'react';
 
 export default React.forwardRef(function DeleteCollectionDialog(
   props,
   ref: Ref<HTMLDivElement>,
 ) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const { toast } = useToast();
+
+  async function handleSubmit(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    await (async () => {
+      return new Promise((resolve, reject) =>
+        setTimeout(() => resolve('resolved'), 1000),
+      );
+    })();
+
+    toast({
+      title: 'Collection successfully deleted.',
+    });
+    setIsOpen(false);
+    setIsLoading(false);
+    router.push('/home');
+  }
+
   return (
-    <AlertDialog>
+    <AlertDialog onOpenChange={(open) => setIsOpen(open)} open={isOpen}>
       <AlertDialogTrigger asChild>
         <DropdownMenuItem
           asChild
@@ -46,7 +73,12 @@ export default React.forwardRef(function DeleteCollectionDialog(
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction asChild>
+            <Button onClick={handleSubmit} disabled={isLoading}>
+              {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+              {isLoading ? 'Deleting...' : 'Continue'}
+            </Button>
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
