f2d44d2deee2856a2e05b46c98b45752012f649d
Peter Pardo <peterpardo123@gmail.com>
2024-02-11 21:34:34 +0800

    feat: updated billing page

--- app/home/billing/page.tsx
+++ app/home/billing/page.tsx
@@ -1,5 +1,24 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 export default function BillingPage() {
-  return <div>Billing</div>;
+  return (
+    <div className="space-y-5">
+      <h1 className="font-bold text-xl md:text-4xl">Subscription</h1>
+
+      <div className="space-y-5 border rounded p-5 bg-slate-50 bg-opacity-5">
+        <div className="space-y-3">
+          <h2 className="text-2xl font-semibold">Curent Plan</h2>
+          <p>
+            You are currently on the{' '}
+            <span className="font-bold">Free Plan</span>. Click the button below
+            to change your plan.
+          </p>
+          <p>- Free plan</p>
+        </div>
+
+        <Button>Change Plan</Button>
+      </div>
+    </div>
+  );
 }
